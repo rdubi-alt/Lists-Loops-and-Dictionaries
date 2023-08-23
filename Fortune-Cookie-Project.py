23e76907834e94a13a0fcfdef68be7c5f2f0c459
rdubi-alt <126214212+rdubi-alt@users.noreply.github.com>
2023-08-22 23:00:40 -0700

    Update and rename 1_FortuneCookie.py to Fortune-Cookie-Project.py

--- Fortune-Cookie-Project.py
+++ Fortune-Cookie-Project.py
@@ -1,21 +1,12 @@
-#make stuff random using python module-google python random to see all
-#python random modules
+#you can make anything random by importing a python module-google "python random" to see all possible modules
 import random
-answer = random.randint(1,3)
-
-if answer == 1:
-    print("Yes")
-if answer == 2:
-    print("No")
-if answer == 3:
-    print("Maybe")
-
-import random
-
+#Generates random "lucky number"
 lucky_number = random.randint(1,100)
+#Generates random integer between 1 and 3 
 fortune_number = random.randint(1,3)
 fortune_text = ""
 
+#based on random fortune number, person will get their fortune told
 if fortune_number == 1:
     fortune_text = "You will have a great day!"
 if fortune_number == 2:
